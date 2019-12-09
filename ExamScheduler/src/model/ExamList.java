8eb82d87497a09cb6ca2698f300d1dbe47c849f0
bandreit <abostan.ir@gmail.com>
2019-12-09 14:38:37 +0100

    solved conflicts

--- ExamScheduler/src/model/ExamList.java
+++ ExamScheduler/src/model/ExamList.java
@@ -15,12 +15,9 @@ public class ExamList
   {
     return exams.size();
   }
-<<<<<<< HEAD
 
   public void addExam(Exam exam)
-=======
-  public void add(Exam exam)
->>>>>>> 50ed1581058d2ef35eab3abbe74bd3a20b913133
+
   {
     exams.add(exam);
   }
