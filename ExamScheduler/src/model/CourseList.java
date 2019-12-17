fa0d836cc5f087ff6f12eecca52a02ee4f388491
bandreit <abostan.ir@gmail.com>
2019-12-17 12:02:45 +0200

    Started implementing the tests

--- ExamScheduler/src/model/CourseList.java
+++ ExamScheduler/src/model/CourseList.java
@@ -11,11 +11,6 @@ public class CourseList
     courses = new ArrayList<>();
   }
 
-  public void addCourse(Course course)
-  {
-    courses.add(course);
-  }
-
   public Course getCourse(int index)
   {
     return courses.get(index);
@@ -33,23 +28,9 @@ public class CourseList
     return null;
   }
 
-  public int size(){return courses.size();}
-
-  public void removeCourse(String name)
+  public int size()
   {
-    for (int i = 0; i < courses.size(); i++)
-    {
-      if (courses.get(i).getName().equals(name))
-      {
-        courses.remove(i);
-        break;
-      }
-    }
-  }
-
-  public ArrayList<Course> getAllCourses()
-  {
-    return courses;
+    return courses.size();
   }
 
   public String toString()
